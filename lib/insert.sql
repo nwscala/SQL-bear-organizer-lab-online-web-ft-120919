336d59945f344d361b995bc0dec01c7ca679f1d4
nwscala <nwscala@gmail.com>
2020-01-21 20:06:43 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 16, "M", "Chartreuse", "Loud", 1),
+("Rowdy", 24, "M", "Heliotrope", "Rowdy", 1),
+("Tabitha", 10, "F", "Blue", "Tattletale", 1),
+("Sargeant Brown", 39, "M", "Vermilion", "Frugal", 1),
+("Melissa", 22, "F", "Orange", "Scheming", 1),
+("Grinch", 30, "M", "Gray", "Festive", 1),
+("Wendy", 36, "F", "White", "Mascot", 1),
+(NULL, 20, "M", "Brown", "Murderous", 0);
